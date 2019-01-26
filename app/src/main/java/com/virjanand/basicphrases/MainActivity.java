15330426b0974fb650cf9c8ec3161fecfb7e5919
Virjanand <virjanand@virjanand.com>
2019-01-26 14:04:10 +0100

    Added method to buttons that plays audio file with same name as in tag

--- app/src/main/java/com/virjanand/basicphrases/MainActivity.java
+++ app/src/main/java/com/virjanand/basicphrases/MainActivity.java
@@ -1,9 +1,22 @@
 package com.virjanand.basicphrases;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void playFrenchTranslation(View view) {
+
+        String tag = (String) view.getTag();
+        playSentence(tag);
+    }
+
+    private void playSentence(String tag) {
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, getResources().getIdentifier(tag, "raw", getPackageName()));
+        mediaPlayer.start();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
